739c6c0e5d20e26af7362f08e03e5608e182dfc9
Eric Endsley <eric.endsley4@gmail.com>
2020-11-06 09:28:24 -0800

    create pizza constructor which passes first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 //Describe: PizzaOrder() 
-//Test: should create a pizza order object with property size equal to size parameter and all toppings set to false.
-//Code: let order = new PizzaOrder(large);
-//Expect: order.orderSize.toEqual("Large");
+//Test: should create a pizza order object with property size equal to size parameter, price set to 0 and all toppings set to false.
+//Code: let order = new PizzaOrder("large");
+//Expect: order.size.toEqual("large");
+
+function PizzaOrder(size) {
+  this.size = size;
+  this.cheese = false;
+  this.pepperoni = false;
+  this.mushrooms = false;
+  this.price = 0;
+} 
+
+let myPizza = new PizzaOrder("large");
+myPizza
