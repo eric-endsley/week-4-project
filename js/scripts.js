767a2a54952fdb47b6ace6f31ade450c048adca5
Eric Endsley <eric.endsley4@gmail.com>
2020-11-06 09:18:50 -0800

    wrote first test for PizzaOrder constructor in scripts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+//Describe: PizzaOrder() 
+//Test: should create a pizza order object with property size equal to size parameter and all toppings set to false.
+//Code: let order = new PizzaOrder(large);
+//Expect: order.orderSize.toEqual("Large");
