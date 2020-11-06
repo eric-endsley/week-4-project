7de7005fcf6d9327d6b9b6b110b89ca8691b1a69
Eric Endsley <eric.endsley4@gmail.com>
2020-11-06 10:54:36 -0800

    write tests for price calculation prototype

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ let myPizza = new PizzaOrder();
 //Describe: addCheese() 
 //Test: should set value of cheese property to true in a PizzaOrder object
 //Code: let newOrder = new PizzaOrder();
-//Code: let newCheeseOrder = newOrder.addCheese
+//Code: let newCheeseOrder = newOrder.addCheese()
 //Expect: newCheeseOrder.cheese.toEqual("true");
 
 PizzaOrder.prototype.addCheese = function() {
@@ -29,25 +29,56 @@ myPizza.cheese
 //Describe: addPep() 
 //Test: should set value of pepperoni property to true in a PizzaOrder object
 //Code: let newOrder = new PizzaOrder();
-//Code: let newPepOrder = newOrder.addPep
+//Code: let newPepOrder = newOrder.addPep()
 //Expect: newPepOrder.pepperoni.toEqual("true");
 
 PizzaOrder.prototype.addPep = function() {
   this.pepperoni = true
 }
 
-myPizza.addCheese();
-myPizza.cheese
+myPizza.addPep();
+myPizza.pepperoni
 
 //Describe: addMush() 
 //Test: should set value of mushrooms property to true in a PizzaOrder object
 //Code: let newOrder = new PizzaOrder();
-//Code: let newMushOrder = newOrder.addCheese
-//Expect: newMushOrder.musshrooms.toEqual("true");
+//Code: let newMushOrder = newOrder.addCheese()
+//Expect: newMushOrder.mushrooms.toEqual("true");
 
 PizzaOrder.prototype.addMush = function() {
   this.mushrooms = true
 }
 
-myPizza.addCheese();
-myPizza.cheese
+myPizza.addMush();
+myPizza.mushrooms
+
+
+//Describe: calcPrice()
+//Test: should set PizzaOrder.price to 16 if pizza size is large
+//Code: let newOrder = new PizzaOrder(large);
+//Code: let orderPrice = newOrder.calcPrice()
+//Expect: orderPrice.price.toEqual(16)
+
+//Test: shoulde set PizzaOrder.price to 10 if pizza size is small
+//Code: let newOrder = new PizzaOrder(small);
+//Code: let orderPrice = newOrder.calcPrice()
+//Expect: orderPrice.price.toEqual(10) 
+
+//Test: should add 1 to price property if PizzaOrder.cheese = true
+//Code: let newOrder = new PizzaOrder(small)
+//Code: let newOrder.cheese = true
+//Code: let newOrderPrice = newOrder.calcPrice()
+//Expect: newOrderPrice.price.toEqual(11)
+
+//Test: should add 3 to price property if PizzaOrder.pepperoni= true
+//Code: let newOrder = new PizzaOrder(small)
+//Code: let newOrder.pepperoni = true
+//Code: let newOrderPrice = newOrder.calcPrice()
+//Expect: newOrderPrice.price.toEqual(13)
+
+//Test: should add 2 to price property if PizzaOrder.m = true
+//Code: let newOrder = newPizzaOrder(small)
+//Code: let newOrder.mushrooms = true
+//Code: let newOrdprice = newOrder.calcPrice()
+//Expect: newOrderPrice.price.toEqual(12)
+
