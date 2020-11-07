8c0fbf444998e83991b0fb7eefc6100b8d623b17
Eric Endsley <eric.endsley4@gmail.com>
2020-11-06 16:23:46 -0800

    add UI logic to take inputs from fancy toppings form, add fancyToppings property to Pizza Order object,  change calcPrice to add fixed value for each selected basic/fancy topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic:
-function PizzaOrder(size, toppings) {
+function PizzaOrder(size, toppings, fancyToppings) {
   this.size = size;
   this.toppings = toppings;
+  this.fancyToppings = fancyToppings;
   this.price = 0;
 } 
 
@@ -10,13 +11,9 @@ PizzaOrder.prototype.calcPrice = function() {
     this.price = 16;
   } else if (this.size ==="small") {
     this.price = 10;
-  } if (this.toppings.includes("cheese")) {
-    this.price += 1
-  } if (this.toppings.includes("pepperoni")) {
-    this.price += 3
-  } if (this.toppings.includes("mushrooms")) {
-    this.price += 2 
-  }
+  } 
+  this.price += 3 * (this.toppings).length;
+  this.price += 5 * (this.fancyToppings).length;
 }
 
 //UI logic:
@@ -26,10 +23,16 @@ $(document).ready(function() {
 
     let sizeInput = $("input:radio[name=size]:checked").val();
     let toppingsInput = []
+    let fancyToppingsInput = []
+
     $("input:checkbox[name=toppings]:checked").each(function() {
-      toppingsInput.push($(this).val())
+      toppingsInput.push($(this).val()) 
     });
-    let newOrder = new PizzaOrder(sizeInput, toppingsInput);
+    $("input:checkbox[name=fancy]:checked").each(function() {
+      fancyToppingsInput.push($(this).val())
+    });
+  
+    let newOrder = new PizzaOrder(sizeInput, toppingsInput, fancyToppingsInput);
     newOrder.calcPrice();
     $(".output").html(`<p class="price">Your pizza will cost you $${newOrder.price}</p>`)
   });
